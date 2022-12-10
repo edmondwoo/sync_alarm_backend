4c8dc3abe02bff2d539714e58d06e39a6aa40ca9
edmondwoo <edmond524524@yahoo.com.hk>
2022-12-10 22:12:17 +0800

    add render.yaml

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_2nx5adHkN315trx5SQ3xF7MtBESl/IM53yY88N6",
+        apiToken: "id6BI2zavdz80nqNrtdhSYPl",
+        appFilter: "sync-alarm-backend",
+        // teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
   },
 });
